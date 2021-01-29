3906f7d902005f30503e346adffd798395c15751
Abdur Nur Tanim <71883296+antanim@users.noreply.github.com>
2021-01-30 02:07:16 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,28 @@
-// age in days project
+// age in days project it's my simple culculator age in days create by abdur nur tanim https://facebook.com/abdurnurtanim11
 
 let check = document.getElementById("check");
 
-    check.addEventListener("click", function (){
+check.addEventListener("click", function() {
 
-        let resultBox = document.getElementById("resultBox");
-        let promptBox = prompt("Enter your brithday year.");
-        let ageInDay = (2020 - promptBox) * 365;
-        let ageInDays = "Your are " + ageInDay + " days old.";
+    let resultBox = document.getElementById("resultBox");
+    let promptBox = prompt("Enter your brithday year.");
+    let ageInDay = (2020 - promptBox);
+    let ageInDays = `Your are ${ageInDay} years old.`
+        // resultBox.innerHTML = ageInDays;
+    if (promptBox === '' || promptBox === null) {
+        alert("Please Enter Your Age!")
+        resultBox.innerHTML = `${promptBox}it's your not Brith year! \n Enter Your Current Brith year`
+    } else {
         resultBox.innerHTML = ageInDays;
+    }
+});
 
-    });
-	
 
 let reset = document.getElementById("reset");
 
-    reset.addEventListener("click", function (){
+reset.addEventListener("click", function() {
 
-        let resultBox = document.getElementById("resultBox");
-        resultBox.innerHTML = "";
+    let resultBox = document.getElementById("resultBox");
+    resultBox.innerHTML = "";
 
-    });
-
-// ORDER COMPLETED
+});
